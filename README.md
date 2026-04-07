aa13be9793e854e4c09ec01db3f8aee5368aa2d1
renzremigio777 <69234830+renzremigio777@users.noreply.github.com>
2026-04-07 12:55:00 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# betzone-template
+
---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+
+window.addEventListener("load", () => {
+  const loadingOverlay = document.getElementById("loading-overlay");
+  const progress = document.getElementById("progress");
+  const progressText = document.getElementById("progress-text");
+  const box = progress.getBBox();
+  let progressValue = 10//box.width;
+
+  let poll = null;
+
+  function startVideo() {
+    const video = document.getElementById('video');
+    const videoSrc = 'http://playertest.longtailvideo.com/adaptive/wowzaid3/playlist.m3u8';
+
+    if (video.canPlayType('application/vnd.apple.mpegurl')) {
+      // Safari
+      video.src = videoSrc;
+    } else if (Hls.isSupported()) {
+      // Other browsers
+      const hls = new Hls();
+      hls.loadSource(videoSrc);
+      hls.attachMedia(video);
+    }
+  }
+  function startProgress() {
+    poll = setInterval(async () => {
+      progressValue += 1;
+      progress.setAttribute("width", progressValue);
+      progressText.innerHTML = progressValue + '%'
+      if (progressValue === 48 || progressValue === 93) {
+        clearInterval(poll);
+        setTimeout(() => {
+          startProgress(); // resume after 5s
+        }, 300);
+      }
+      else if (progressValue >= 100) {
+        clearInterval(poll);
+        console.log('Complete');
+        loadingOverlay.classList.add('fading');
+        setTimeout(() => { loadingOverlay.remove() }, 900);
+        startVideo();
+      }
+    }, 2);
+  }
+
+  startProgress();
+
+  
+});
